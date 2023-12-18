9e0e968c7586ca53a28394d4c507d7242568838f
MariosCode <MarioCorteguera@gmail.com>
2023-12-18 14:43:33 -0500

    set up for output management with webpack

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 import Notes from './data.csv';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
     // Lodash is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -20,6 +22,10 @@ function component() {
 
     console.log(Data);
     console.log(Notes);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
   
     return element;
   }
